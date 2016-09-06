b9791d758b524688a6d640141422c9c25aa0e243
千橙 <mgr.qiancheng@gmail.com>
2016-09-06 17:34:58 +0800

    shard interceptor

---
+++ src/main/java/com/challions/dao/shardbatis/interceptor/ShardInterceptor.java
@@ -0,0 +1,28 @@
+package com.challions.dao.shardbatis.interceptor;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+
+import java.util.Properties;
+
+/**
+ * 分片拦截器
+ * @author qian.cheng
+ */
+public class ShardInterceptor implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object plugin(Object o) {
+        return null;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
